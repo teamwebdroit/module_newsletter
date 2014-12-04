0fa1eedabac023927762e38235a186f452d44a22
Cindy <cindy.leschaud@gmail.com>
2014-12-04 09:17:30 +0100

    try pass path for upload

--- public/admin/js/files.js
+++ public/admin/js/files.js
@@ -413,9 +413,14 @@ $(function(){
 
     startFileManager();
 
-    /***********************
+    /******************************
+     * Add input for url hash
+     ******************************/
+
+
+    /******************************
      * Upload manager
-    ************************/
+    *******************************/
 
     // Change this to the location of your server-side upload handler:
     var url = 'uploadJquery',
@@ -423,8 +428,7 @@ $(function(){
             .addClass('btn btn-primary btn-sm').prop('disabled', true).text('Processing...')
             .on('click', function () {
                 var $this = $(this),
-                    data = $this.data();
-
+                    data  = $this.data();
                 $this.off('click').text('Abort').on('click', function () {
                     $this.remove();
                     data.abort();
@@ -439,6 +443,7 @@ $(function(){
         url: url,
         dataType: 'json',
         autoUpload: false,
+        singleFileUploads:true,
         acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
         maxFileSize: 5000000, // 5 MB
         // Enable image resizing, except for Android and Opera,
@@ -449,65 +454,77 @@ $(function(){
         previewMaxWidth: 100,
         previewMaxHeight: 90,
         previewCrop: true
+
     }).on('fileuploadadd', function (e, data) {
+
         data.context = $('<div/>').appendTo('#files');
+
         $.each(data.files, function (index, file) {
-            var node = $('<p/>')
-                .append($('<span/>').text(file.name));
+
+            var node = $('<p/>').append($('<span/>').text(file.name));
             if (!index) {
                 node.append(uploadButton.clone(true).data(data));
             }
             node.appendTo(data.context);
+
         });
+
     }).on('fileuploadprocessalways', function (e, data) {
+
         var index = data.index,
-            file = data.files[index],
-            node = $(data.context.children()[index]);
+            file  = data.files[index],
+            node  = $(data.context.children()[index]);
+
         if (file.preview) {
             node.prepend(file.preview);
         }
+
         if (file.error) {
             node.append($('<span class="text-danger"/>').text(file.error));
         }
+
         if (index + 1 === data.files.length) {
             data.context.find('button')
                 .text('Envoyer')
                 .prop('disabled', !!data.files.error);
         }
+
     }).on('fileuploadprogressall', function (e, data) {
+
         var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
+        $('#progress .progress-bar').css( 'width',progress + '%' );
+
     }).on('fileuploaddone', function (e, data) {
+
         $.each(data.result.files, function (index, file) {
             if (file.url)
             {
                 var link = $('<a>').attr('target', '_blank').prop('href', file.url);
-                $(data.context.children()[index]) .wrap(link);
+                $(data.context.children()[index]).wrap(link);
+                console.log(data.context.children()[index]);
             }
             else if (file.error)
             {
                 var error = $('<span class="text-danger"/>').text(file.error);
                 $(data.context.children()[index]).append(error);
             }
+
         });
 
-        $("#files").empty();
+        //$("#files").empty();
         //$('#progress .progress-bar').hide(1000);
         // Reload filemanager
         startFileManager();
 
     }).on('fileuploadfail', function (e, data) {
-        $.each(data.files, function (index) {
-            var error = $('<span class="text-danger"/>').text('File upload failed.');
-            $(data.context.children()[index])
-                .append('<br>')
-                .append(error);
-        });
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
 
+        $.each(data.files, function (index) {
+
+            var error = $('<span class="text-danger"/>').text('File upload failed.');
+            $(data.context.children()[index]).append(error);
+
+        });
+
+    }).prop('disabled', !$.support.fileInput).parent().addClass($.support.fileInput ? undefined : 'disabled');
 
 });
