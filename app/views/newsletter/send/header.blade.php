5cbb8df776b4113b910a62cf3e88127071e30caa
Cindy <cindy.leschaud@gmail.com>
2014-12-10 16:35:41 +0100

    change color logo in newsletter header

--- app/views/newsletter/send/header.blade.php
+++ app/views/newsletter/send/header.blade.php
@@ -2,20 +2,20 @@
     <td align="center">
 
         <!-- Title -->
-        <table width="600" bgcolor="#676767" border="0" cellpadding="0" cellspacing="0" align="center" class="resetTable headerInfos">
-            <tr bgcolor="#676767"><td colspan="3" height="10"></td></tr><!-- space -->
-            <tr bgcolor="#676767">
+        <table width="600" bgcolor="#5a101f" border="0" cellpadding="0" cellspacing="0" align="center" class="resetTable headerInfos">
+            <tr bgcolor="#5a101f"><td colspan="3" height="10"></td></tr><!-- space -->
+            <tr bgcolor="#5a101f">
                 <td width="20"></td>
                 <td align="left"><h1 class="header">{{ $infos->sujet  }}</h1></td>
                 <td width="20"></td>
             </tr>
 
-            <tr bgcolor="#676767">
+            <tr bgcolor="#5a101f">
                 <td width="20"></td>
                 <td align="left"><h2 class="header headerSmall">Editée par {{ $infos->auteurs }}</h2></td>
                 <td width="20"></td>
             </tr>
-            <tr bgcolor="#676767"><td colspan="3" height="10"></td></tr><!-- space -->
+            <tr bgcolor="#5a101f"><td colspan="3" height="10"></td></tr><!-- space -->
         </table>
         <!-- End title -->
 
