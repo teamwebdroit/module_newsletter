468e54372ce3efe7c9632d5f00604f7341f4b06f
Cindy <cindy.leschaud@gmail.com>
2014-12-16 09:53:24 +0100

    rename of template right send

--- app/views/newsletter/send/ImageRightText.blade.php
+++ app/views/newsletter/send/ImageRightText.blade.php
@@ -5,7 +5,6 @@
     </tr><!-- space -->
     <tr align="center" class="resetMarge">
         <td class="resetMarge">
-
             <!-- Bloc content-->
             <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="tableReset contentForm">
                 <tr>
@@ -24,7 +23,6 @@
                 </tr>
             </table>
             <!-- Bloc content-->
-
         </td>
     </tr>
     <tr bgcolor="ffffff"><td colspan="3" height="35" class="blocBorder"></td></tr><!-- space -->
