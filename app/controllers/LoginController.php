3f897082c0a52b859c16fcd8fa8e06ddb5b6c7a6
Cindy <cindy.leschaud@gmail.com>
2015-01-15 16:41:52 +0100

    remove filter in login

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -2,10 +2,6 @@
 
 class LoginController extends \BaseController {
 
-	public function __construct()
-	{
-		$this->beforeFilter('csrf', array('only' => array('store')));
-	}
 	/**
 	 * Display a listing of the resource.
 	 * GET /login
