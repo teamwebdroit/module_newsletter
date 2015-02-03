978fba09c6d09e0273dba810a746f68034a0059d
Cindy <cindy.leschaud@gmail.com>
2015-02-03 08:28:54 +0100

    add analyses to newsletter page on arret

--- app/views/content/arret.blade.php
+++ app/views/content/arret.blade.php
@@ -8,6 +8,9 @@
             </div><!--END POST-TITLE-->
             <div class="post-entry">
                 {{ $bloc->pub_text }}
+                @if(isset($bloc->file))
+                    <p><a target="_blank" href="{{ asset('files/arrets/'.$bloc->file) }}">Télécharger en pdf</a></p>
+                @endif
             </div>
         </div><!--END POST-->
     </div>
@@ -24,3 +27,34 @@
         ?>
     </div>
 </div>
+
+@if(!$bloc->arrets_analyses->isEmpty())
+
+<!-- Bloc content-->
+<div class="row">
+    <div class="col-md-9">
+
+        @foreach($bloc->arrets_analyses as $analyse)
+            <div class="post">
+                    <div class="post-title">
+                        <?php setlocale(LC_ALL, 'fr_FR.UTF-8');  ?>
+                        <h2 class="title">Analyse de l'arrêt {{ $bloc->reference }}</h2>
+                    </div><!--END POST-TITLE-->
+                    <div class="post-entry">
+                        <h4>{{ $analyse->authors }}</h4>
+                        <p>{{ $analyse->abstract }}</p>
+                        <p><a href="{{ asset('files/analyses/'.$analyse->file) }}">Télécharger en pdf</a></p>
+                    </div>
+            </div><!--END POST-->
+        @endforeach
+
+    </div>
+    <div class="col-md-3 listCat">
+        <a target="_blank" href="{{ url('jurisprudence') }}">
+            <img width="130" border="0" alt="Analyse" src="{{ asset('images/analyse.png') }}">
+        </a>
+    </div>
+    <!-- Bloc content-->
+
+</div>
+@endif
