31cb8bbba99007cec3e4167568ff13a998365964
Cindy <cindy.leschaud@gmail.com>
2015-05-20 15:40:12 +0200

    table for fix image gap

--- app/views/newsletter/send/arret.blade.php
+++ app/views/newsletter/send/arret.blade.php
@@ -24,7 +24,7 @@
                            echo '<table border="0" width="160" align="center" cellpadding="0" cellspacing="0">';
                            foreach($bloc->arrets_categories as $categorie)
                            {
-                               echo '<tr align="center" style="margin: 0;padding: 0;"><td style="margin: 0;padding: 0;">';
+                               echo '<tr align="center" style="margin: 0;padding: 0;"><td style="margin: 0;padding: 0;page-break-before: always;">';
                                // Categories
                                echo '<a target="_blank" href="'.url('jurisprudence').'#'.$bloc->reference.'" style="margin:0;padding:0;display: block;">
                                         <img width="130" height="158" style="margin:0;padding:0;display: block;" border="0" alt="'.$categorie->title.'" src="'.asset('newsletter/pictos/'.$categorie->image).'">
