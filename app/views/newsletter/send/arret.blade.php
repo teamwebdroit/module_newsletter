337c7cf2dd5bc9e8d11e23fb4d8d80c8427c58f5
Cindy <cindy.leschaud@gmail.com>
2015-06-17 08:29:15 +0200

    name author sans serif

--- app/views/newsletter/send/arret.blade.php
+++ app/views/newsletter/send/arret.blade.php
@@ -65,12 +65,12 @@
                                 <table border="0" width="375" align="left" cellpadding="0" cellspacing="0" class="resetTable">
                                     <tr>
                                         <td valign="top" width="60" class="resetMarge">
-                                            <img width="60" border="0" alt="{{ $analyse->auteur->name }}" src="{{ asset('authors/'.$analyse->auteur->photo) }}">
+                                            <img style="width: 60px;" width="60" border="0" alt="{{ $analyse->auteur->name }}" src="{{ asset('authors/'.$analyse->auteur->photo) }}">
                                         </td>
                                         <td valign="top" width="10" class="resetMarge"></td>
                                         <td valign="top" width="305" class="resetMarge">
-                                            <h3 style="text-align: left;">{{ $analyse->auteur->name }}</h3>
-                                            <p style="text-align: left;">{{ $analyse->auteur->occupation }}</p>
+                                            <h3 style="text-align: left;font-family: sans-serif;">{{ $analyse->auteur->name }}</h3>
+                                            <p style="text-align: left;font-family: sans-serif;">{{ $analyse->auteur->occupation }}</p>
                                         </td>
                                     </tr>
                                     <tr bgcolor="ffffff"><td colspan="3" height="15" class=""></td></tr><!-- space -->
