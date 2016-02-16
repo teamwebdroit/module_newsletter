6072ff9362e0e24f0ff7c649823dd38a243c7671
Cindy <cindy.leschaud@gmail.com>
2016-02-16 08:35:15 +0100

    fix tables and delimiter newsletter for campagne

--- app/views/newsletter/send/arret.blade.php
+++ app/views/newsletter/send/arret.blade.php
@@ -5,6 +5,7 @@
     </tr><!-- space -->
     <tr align="center" class="resetMarge">
         <td class="resetMarge">
+
             <!-- Bloc content-->
             <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="tableReset contentForm">
                 <tr>
@@ -43,68 +44,69 @@
         </td>
     </tr>
     <tr bgcolor="ffffff"><td colspan="3" height="35" class="blocBorder"></td></tr><!-- space -->
-</table>
-<!-- End bloc -->
 
-@if(isset($bloc->analyses))
-    <!-- Bloc content-->
-    <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="resetTable">
-        <tr bgcolor="ffffff">
-            <td colspan="3" height="35"></td>
-        </tr><!-- space -->
-        <tr>
-            <td valign="top" width="375" class="resetMarge contentForm">
-                <?php $i = 1; ?>
-                @foreach($bloc->analyses as $analyse)
-                    <table border="0" width="375" align="left" cellpadding="0" cellspacing="0" class="resetTable">
-                        <tr>
-                            <td valign="top" width="375" class="resetMarge contentForm">
-                                <?php setlocale(LC_ALL, 'fr_FR.UTF-8');  ?>
-                                <h3 style="text-align: left;font-family: sans-serif;">Analyse de l'arrêt {{ $bloc->reference }}</h3>
+    @if(isset($bloc->analyses))
+        <tr align="center" class="resetMarge">
+            <td class="resetMarge">
 
-                                    @if(isset($analyse->analyse_authors))
-                                        @foreach($analyse->analyse_authors as $analyse_authors)
-                                            <table border="0" width="375" align="left" cellpadding="0" cellspacing="0" class="resetTable">
-                                                <tr>
-                                                    <td valign="top" width="60" class="resetMarge">
-                                                        <img style="width: 60px;" width="60" border="0" alt="{{ $analyse_authors->name }}" src="{{ asset('authors/'.$analyse_authors->photo) }}">
-                                                    </td>
-                                                    <td valign="top" width="10" class="resetMarge"></td>
-                                                    <td valign="top" width="305" class="resetMarge">
-                                                        <h3 style="text-align: left;font-family: sans-serif;">{{ $analyse_authors->name }}</h3>
-                                                        <p style="text-align: left;font-family: sans-serif;">{{  $analyse_authors->occupation }}</p>
-                                                    </td>
-                                                </tr>
-                                                <tr bgcolor="ffffff"><td colspan="3" height="15" class=""></td></tr><!-- space -->
-                                            </table>
-                                        @endforeach
+                <!-- Bloc content-->
+                <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="resetTable">
+                    <tr bgcolor="ffffff"><td colspan="3" height="35"></td></tr><!-- space -->
+                    <tr>
+                        <td valign="top" width="375" class="resetMarge contentForm">
+                            <?php $i = 1; ?>
+                            @foreach($bloc->analyses as $analyse)
+                                <table border="0" width="375" align="left" cellpadding="0" cellspacing="0" class="resetTable">
+                                    <tr>
+                                        <td valign="top" width="375" class="resetMarge contentForm">
+                                            <?php setlocale(LC_ALL, 'fr_FR.UTF-8');  ?>
+                                            <h3 style="text-align: left;font-family: sans-serif;">Analyse de l'arrêt {{ $bloc->reference }}</h3>
+
+                                                @if(isset($analyse->analyse_authors))
+                                                    @foreach($analyse->analyse_authors as $analyse_authors)
+                                                        <table border="0" width="375" align="left" cellpadding="0" cellspacing="0" class="resetTable">
+                                                            <tr>
+                                                                <td valign="top" width="60" class="resetMarge">
+                                                                    <img style="width: 60px;" width="60" border="0" alt="{{ $analyse_authors->name }}" src="{{ asset('authors/'.$analyse_authors->photo) }}">
+                                                                </td>
+                                                                <td valign="top" width="10" class="resetMarge"></td>
+                                                                <td valign="top" width="305" class="resetMarge">
+                                                                    <h3 style="text-align: left;font-family: sans-serif;">{{ $analyse_authors->name }}</h3>
+                                                                    <p style="text-align: left;font-family: sans-serif;">{{  $analyse_authors->occupation }}</p>
+                                                                </td>
+                                                            </tr>
+                                                            <tr bgcolor="ffffff"><td colspan="3" height="15" class=""></td></tr><!-- space -->
+                                                        </table>
+                                                    @endforeach
+                                                @endif
+
+                                                <p class="abstract">{{ $analyse->abstract }}</p>
+                                            <p><a href="{{ asset('files/analyses/'.$analyse->file) }}">Télécharger en pdf</a></p>
+                                        </td>
+                                    </tr>
+
+                                    @if( $bloc->analyses->count() > 1 && $bloc->analyses->count() > $i)
+                                        <tr bgcolor="ffffff"><td colspan="3" height="35" class=""></td></tr><!-- space -->
                                     @endif
 
-                                    <p class="abstract">{{ $analyse->abstract }}</p>
-                                <p><a href="{{ asset('files/analyses/'.$analyse->file) }}">Télécharger en pdf</a></p>
-                            </td>
-                        </tr>
+                                    <?php $i++; ?>
+                                </table>
+                            @endforeach
 
-                        @if( $bloc->analyses->count() > 1 && $bloc->analyses->count() > $i)
-                            <tr bgcolor="ffffff"><td colspan="3" height="35" class=""></td></tr><!-- space -->
-                        @endif
+                        </td>
+                        <td width="25" class="resetMarge"></td><!-- space -->
+                        <td align="center" valign="top" width="160" class="resetMarge">
+                            <!-- Categories -->
+                            <div class="resetMarge">
+                                <a target="_blank" href="{{ url('jurisprudence') }}"><img width="130" border="0" alt="Analyse" src="{{ asset('images/analyse.png') }}"></a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
 
-                        <?php $i++; ?>
-                    </table>
-                @endforeach
-
-            </td>
-            <td width="25" class="resetMarge"></td><!-- space -->
-            <td align="center" valign="top" width="160" class="resetMarge">
-                <!-- Categories -->
-                <div class="resetMarge">
-                    <a target="_blank" href="{{ url('jurisprudence') }}">
-                        <img width="130" border="0" alt="Analyse" src="{{ asset('images/analyse.png') }}">
-                    </a>
-                </div>
             </td>
         </tr>
         <tr bgcolor="ffffff"><td colspan="3" height="35" class="blocBorder"></td></tr><!-- space -->
-    </table>
-    <!-- Bloc content-->
-@endif
+    @endif
+</table>
+<!-- End bloc -->
--- app/views/newsletter/send/groupe.blade.php
+++ app/views/newsletter/send/groupe.blade.php
@@ -1,63 +1,74 @@
 @if(isset($bloc->arrets))
 
-    <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="resetTable">
-        <tr bgcolor="ffffff">
-            <td colspan="3" height="35"> </td>
-        </tr><!-- space -->
-        <tr bgcolor="ffffff" class="blocBorder">
-            <td width="400" align="left" class="resetMarge contentForm" valign="top">
-                <h3 style="text-align: left;font-family: sans-serif;">{{ $allcategories[$bloc->categorie] }}</h3>
-            </td>
-            <td width="160" align="center" valign="top" class="resetMarge">
-                <img width="130" border="0" src="{{ asset('newsletter/pictos/'.$bloc->image) }}" alt="{{ $allcategories[$bloc->categorie] }}" />
-            </td>
-        </tr><!-- space -->
-    </table>
-
-    <!-- Bloc content-->
+<!-- Bloc -->
+<table border="0" width="600" align="center" cellpadding="0" cellspacing="0" class="tableReset">
+    <tr bgcolor="ffffff"><td colspan="3" height="35"></td></tr><!-- space -->
+    <tr align="center" class="resetMarge">
+        <td class="resetMarge">
+            <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="resetTable">
+                <tr bgcolor="ffffff"><td colspan="3" height="20"></td></tr><!-- space -->
+                <tr bgcolor="ffffff">
+                    <td width="400" align="left" class="resetMarge contentForm" valign="top">
+                        <h3 style="text-align: left;font-family: sans-serif;">{{ $allcategories[$bloc->categorie] }}</h3>
+                    </td>
+                    <td width="160" align="center" valign="top" class="resetMarge">
+                        <img width="130" border="0" src="{{ asset('newsletter/pictos/'.$bloc->image) }}" alt="{{ $allcategories[$bloc->categorie] }}" />
+                    </td>
+                </tr><!-- space -->
+            </table>
+        </td>
+    </tr>
+    <tr bgcolor="ffffff"><td colspan="3" height="20" class="blocBorder"></td></tr><!-- space -->
 
     @foreach($bloc->arrets as $arret)
 
-        <!-- Bloc content-->
-        <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="resetTable">
-            <tr bgcolor="ffffff"><td colspan="3" height="35"></td></tr><!-- space -->
-            <tr>
-                <td valign="top" width="375" class="resetMarge contentForm">
-                    <div>
-                        <?php setlocale(LC_ALL, 'fr_FR.UTF-8');?>
-                        <h3 style="text-align: left;font-family: sans-serif;">{{ $arret->reference }} du {{ $arret->pub_date->formatLocalized('%d %B %Y') }}</h3>
-                        <p class="abstract">{{ $arret->abstract }}</p>
-                        <div>{{ $arret->pub_text }}</div>
-                        <p><a href="{{ asset('files/arrets/'.$arret->file) }}">Télécharger en pdf</a></p>
-                    </div>
-                </td>
-                <td width="25" height="1" class="resetMarge" valign="top" style="font-size: 1px; line-height: 1px;margin: 0;padding: 0;"></td><!-- space -->
-                <td align="center" valign="top" width="160" class="resetMarge">
-                    <!-- Categories -->
-                    <div class="resetMarge">
-                        <?php
-                        if(!$arret->arrets_categories->isEmpty() )
-                        {
-                            echo '<table border="0" width="160" align="center" cellpadding="0" cellspacing="0">';
-                            foreach($arret->arrets_categories as $categorie)
-                            {
-                                if($categorie->id != $bloc->categorie){
-                                    echo '<tr align="center" style="margin: 0;padding: 0;"><td style="margin: 0;padding: 0;page-break-before: always;" valign="top">';
-                                    echo '<a target="_blank" href="'.url('jurisprudence').'#'.$arret->reference.'" style="margin:0;padding:0;display: block;">
-                                            <img style="margin:0;padding:0;display: block;" width="130" height="158" border="0" alt="'.$categorie->title.'" src="'.asset('newsletter/pictos/'.$categorie->image).'">
-                                        </a>';
-                                    echo '</td></tr>';
-                                }
-                            }
-                            echo '</table>';
-                        }
-                        ?>
-                    </div>
-                </td>
-            </tr>
-            <tr bgcolor="ffffff"><td colspan="3" height="35" class="blocBorder"></td></tr><!-- space -->
-        </table>
-        <!-- Bloc content-->
+        <tr align="center" class="resetMarge">
+            <td class="resetMarge">
 
+                <!-- Bloc content-->
+                <table border="0" width="560" align="center" cellpadding="0" cellspacing="0" class="resetTable">
+                    <tr bgcolor="ffffff"><td colspan="3" height="35"></td></tr><!-- space -->
+                    <tr>
+                        <td valign="top" width="375" class="resetMarge contentForm">
+                            <div>
+                                <?php setlocale(LC_ALL, 'fr_FR.UTF-8');?>
+                                <h3 style="text-align: left;font-family: sans-serif;">{{ $arret->reference }} du {{ $arret->pub_date->formatLocalized('%d %B %Y') }}</h3>
+                                <p class="abstract">{{ $arret->abstract }}</p>
+                                <div>{{ $arret->pub_text }}</div>
+                                <p><a href="{{ asset('files/arrets/'.$arret->file) }}">Télécharger en pdf</a></p>
+                            </div>
+                        </td>
+                        <td width="25" height="1" class="resetMarge" valign="top" style="font-size: 1px; line-height: 1px;margin: 0;padding: 0;"></td><!-- space -->
+                        <td align="center" valign="top" width="160" class="resetMarge">
+                            <!-- Categories -->
+                            <div class="resetMarge">
+                                <?php
+                                if(!$arret->arrets_categories->isEmpty() )
+                                {
+                                    echo '<table border="0" width="160" align="center" cellpadding="0" cellspacing="0">';
+                                    foreach($arret->arrets_categories as $categorie)
+                                    {
+                                        if($categorie->id != $bloc->categorie){
+                                            echo '<tr align="center" style="margin: 0;padding: 0;"><td style="margin: 0;padding: 0;page-break-before: always;" valign="top">';
+                                            echo '<a target="_blank" href="'.url('jurisprudence').'#'.$arret->reference.'" style="margin:0;padding:0;display: block;">
+                                                    <img style="margin:0;padding:0;display: block;" width="130" height="158" border="0" alt="'.$categorie->title.'" src="'.asset('newsletter/pictos/'.$categorie->image).'">
+                                                </a>';
+                                            echo '</td></tr>';
+                                        }
+                                    }
+                                    echo '</table>';
+                                }
+                                ?>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr bgcolor="ffffff"><td colspan="3" height="35" class="blocBorder"></td></tr><!-- space -->
     @endforeach
+
+</table>
+<!-- End bloc -->
+
 @endif
